fdd91878c5556a8b80b90c5a20991b4e9875b4c0
Ankit Jain <ankitjain@mail.missouri.edu>
2022-11-05 18:34:01 -0500

    fahjdsfhdaskjldfh

--- api/car_api.py
+++ api/car_api.py
@@ -21,7 +21,7 @@ def get_models(make):
     map(str, models_array)
     return models_array
 
-models_array = get_models("Cadillac")
+#models_array = get_models("Cadillac")
 #print(models_array)
 """ models_array, df_cars = get_models('Toyota')
 print(df_cars)
@@ -42,14 +42,16 @@ print(years_array)  """
 
 def get_mpg_fueltype(year, model):
     df_cars = create_df_cars()
-    df_model = df_cars.loc[df_cars['model'] == model]
-    df_unique_year = df_model.loc[df_cars['year'] == year]
-    df_unique_year.groupby('year').mean()
-    mpg = df_unique_year['comb08'].iloc[0]
+    df_cars = df_cars[['comb08', 'fuelType1', 'make', 'model', 'year']]
+    df_model = df_cars.loc[df_cars['model'] == 'Corolla']
+    df_unique_year = df_model.loc[df_cars['year'] == 2020]
     fueltype = df_model['fuelType1'].iloc[0]
+    df_unique_year = df_unique_year.groupby('year').mean()
+    mpg = df_unique_year['comb08'].iloc[0]
     return float(mpg), str(fueltype)
 
 """ mpg, fueltype = get_mpg_fueltype(1993, df_model)
 print(mpg)
 print(fueltype)  """
+
 
