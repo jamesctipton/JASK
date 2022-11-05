a4f6850d0f3349abb8f3754a45e944020d8222b5
james tipton <jctppc@umsystem.edu>
2022-11-05 15:13:33 -0500

    jank ass path lookin ass

--- pages/History.js
+++ pages/History.js
@@ -2,11 +2,10 @@ import React from "react";
 import { Button, Flex, Text, View } from "@react-native-material/core";
 import { Friends } from "./Friends";
 
-export const History = ({ state, handleChange }) => {
+export const History = () => {
     return (
         <Flex>
             <Text>History</Text>  
-            <Button title="Change State of App" onPress={() => handleChange(<Friends />)}></Button>    
         </Flex>
     )
 }
