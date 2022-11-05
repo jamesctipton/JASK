28cb21a37eecee028af833794d490c44e817e899
james tipton <jctppc@umsystem.edu>
2022-11-04 20:21:04 -0500

    pages

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import GetLocation from 'react-native-get-location';
 
 export default function App() {
   return (
