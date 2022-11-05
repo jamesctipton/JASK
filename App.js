452312c3c6c27b3bee69a730b7b2adcde4c90041
Kent Studer <personal@Kents-MacBook-Pro.local>
2022-11-05 02:01:37 -0500

    rerouting profile

--- App.js
+++ App.js
@@ -15,7 +15,6 @@ import { Home } from './pages/Home';
 import { TripHome } from './pages/TripHome';
 import { History } from './pages/History';
 import { Friends } from './pages/Friends';
-import { Profile } from './pages/Profile';
 
 
 export default function App() {
@@ -27,12 +26,12 @@ export default function App() {
         variant='bottom'
         color='#4caf50'
         leading={ 
-          <Button title="Start Trip" variant='outlined' color='black' trailing={<IconButton disabled icon={<AntDesign name="car" size={30} color="black" />} />} onPress={() => setScreen(<TripHome />)}/> }
+          <Button title="Start Trip" variant='outlined' color='black' trailing={<AntDesign name="car" size={30} color="black" />} onPress={() => setScreen(<TripHome />)}/> }
         trailing={
           <HStack spacing={10}>
             <IconButton icon={<AntDesign name="database" size={34} color="black" />} onPress={() => setScreen(<History state={screen} handleChange={setScreen} />)} />
             <IconButton icon={<AntDesign name="team" size={38} color="black" />} onPress={() => setScreen(<Friends />)}/>  
-            <IconButton icon={<AntDesign name="user" size={38} color="black" />} onPress={() => setScreen(<Profile />)} /> 
+            <IconButton icon={<AntDesign name="user" size={38} color="black" />} onPress={() => setScreen(<Home />)} /> 
           </HStack> 
           }
         style={{ position: "absolute", start: 0, end: 0, bottom: 0, height: 85, justifyContent: "center", paddingLeft: 20, paddingRight: 20 }}>
