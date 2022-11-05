dd854df29780c81d490bf94a51f2f7a2333e7576
Kent Studer <personal@Kents-MacBook-Pro.local>
2022-11-05 12:13:33 -0500

    rebase

--- api/handlers.py
+++ api/handlers.py
@@ -13,31 +13,17 @@ class CarMakeHandler(SessionMixin, RequestHandler):
         self.write(
             json.dumps(makes_array)
         )
-    async def post(self):
-        if not make:
-            error_response = {
-                'resultStatus': 'FAILURE', 
-                'message': 'Car make not provided.'
-            }
-            self.write(error_response)
-        make = self.get_argument("make")
 
 class CarModelHandler(SessionMixin, RequestHandler):
-    def get(self):
-        models_array = get_models(make)
-        
-        self.write(
-            json.dumps(models_array)
-        )
     async def post(self):
-        with self.make_session() as session:
-            if not make:
-                error_response = {
-                    'resultStatus': 'FAILURE', 
-                    'message': 'Car model not provided.'
-                }
-            self.write(error_response)
-        model = self.get_argument("model")
+        data = json.loads(self.request.body)
+        make = data['make']
+        print(make)
+
+        models_array = get_models(make)
+        print(models_array)
+        self.write(json.dumps(models_array))
+        
 
 class CarTypeHandler(SessionMixin, RequestHandler):
     def get(self):
--- pages/Home.js
+++ pages/Home.js
@@ -8,7 +8,7 @@ import { AntDesign } from '@expo/vector-icons';
 
 const dropdownStyle = {
     width: '80%',
-    height: 50,
+    height: 300,
     backgroundColor: '#FFF',
     paddingHorizontal: 0,
     borderWidth: 1,
@@ -34,22 +34,43 @@ export const Home = ({ state, handleChange }) => {
             useNativeDriver: true })
     ).start();
 
-    const [carSpecs, setCarSpecs] = useState({
+
+    let make_data = new Array();
+    let model_data = new Array();
+    let year_data = new Array();
+    let car_data = {
         make: "",
         model: "",
-        year: 0,
-    })
+        year: ""
+    }
 
-    const handleSubmit = () => {
-        return fetch(url)
-        .then((response) => response.json())
-        .then((json) => {
-            console.log(json)
+    fetch(url + '/make').then((response) => 
+        response.json()).then((json) => {
+            for (let index = 0; index < json.length; index++) {
+                make_data.push(json[index])
+            }
         })
         .catch((error) => {
             console.error(error);
-        });
-    };
+    });
+
+    const fetchModels = (car_make) => {
+        console.log(car_make)
+        fetch(url + '/model', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plain, */*', 
+                'Content-Type': 'application/json'
+              },
+            body: JSON.stringify({make: car_make})
+         }).then((response) => 
+            response.json()).then((json) => {
+                console.log(json)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
 
     return (
         <Stack style={{ alignItems: 'center', marginTop: -75}} spacing={20} >
@@ -59,6 +80,7 @@ export const Home = ({ state, handleChange }) => {
             <Text variant="h4" style={{ marginTop: 30 }}>Welcome to E-Mission!</Text>
             <Text variant="h7">Please enter your cars make, model, and year</Text>
             <SelectDropdown 
+                data={make_data}
                 buttonStyle={{ borderRadius: 15, borderWidth: 2, borderColor: '#4caf50', width: '80%', marginBottom: 20}}
                 dropdownStyle={dropdownStyle}
                 defaultButtonText="Make"
@@ -67,8 +89,12 @@ export const Home = ({ state, handleChange }) => {
                 }}
                 dropdownIconPosition={"right"}
                 buttonTextStyle={{ marginLeft: 20 }}
-                onSelect={(e) => setCarSpecs({...carSpecs, make: e.target.value})}
-            />
+                onSelect={(selectedItem, index) => {
+                    car_data.make = selectedItem
+                    fetchModels(selectedItem)
+                }}
+                
+            /> 
             <SelectDropdown 
                 buttonStyle={{ borderRadius: 15, borderWidth: 2, borderColor: '#4caf50', width: '80%', marginBottom: 20}}
                 dropdownStyle={dropdownStyle}
@@ -91,7 +117,7 @@ export const Home = ({ state, handleChange }) => {
                 buttonTextStyle={{ marginLeft: 20 }}
                 onSelect={(e) => setCarSpecs({...carSpecs, year: e.target.value})}
             />
-            <Button title="Submit" variant="contained" color="#4caf50" width={150} onPress={handleSubmit} />
+            <Button title="Submit" variant="contained" color="#4caf50" width={150} onPress={() => {}} />
         </Stack>
     )  
 }
