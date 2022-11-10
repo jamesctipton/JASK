f63b3ce41e8ab27624db35b14624b08fc6802d4d
james tipton <jctppc@umsystem.edu>
2022-11-10 01:44:09 -0600

    slight css

--- RootNavigation.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { createNavigationContainerRef } from '@react-navigation/native';
-
-export const navigationRef = createNavigationContainerRef()
-
-export function navigate(name, params) {
-  navigationRef.current?.navigate(name, params);
-}
